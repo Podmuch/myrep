32ff7e8c9f21242eed8ab6703532fcb457639207
Dominik <dominikpielak@gmail.com>
2014-05-12 19:29:19 +0200

    baza danych

--- klient/klient/klient.cpp
+++ klient/klient/klient.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 int main()
 {
-	printf("Czolgi\n");
+	/*printf("Czolgi\n");
 	printf("Jezeli chcesz polaczyc sie z serwerem podaj ip: ");
 	std::string ip;
 	std::cin >> ip;
 	printf("Podaj port: ");
 	int port;
 	std::cin >> port;
-	Gra gra((char*)ip.c_str(), port);
+	Gra gra((char*)ip.c_str(), port);*/
+	Gra gra("127.0.0.1", 1337);
     return 0;
 }
--- klient/serwer/serwer.cpp
+++ klient/serwer/serwer.cpp
@@ -14,67 +14,66 @@
 HWND nextWnd = 0;
 int counter = 0;
 bool monitoring = true;
-
+char mapa[170];
 int msgcnt = 0;
 
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-	int i;
-	for (i = 0; i<argc; i++){
+	int i=0;
+	/*for (i = 0; i<argc; i++){
 		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
 	}
-	printf("\n");
+	printf("\n");*/
+	strcpy(mapa, argv[0]);
 	return 0;
 }
 
 int main()
 {
-	/*const int LICZBA_GRACZY = 2;
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int  rc;
+	char *sql;
+	struct sockaddr_in sc;
+	int lenc;
+	const int LICZBA_GRACZY = 2;
 	WSADATA wsas;
+	int port;
 	int result;
 	WORD wersja;
+	SOCKET s;
+	struct sockaddr_in sa;
+	SOCKET si[LICZBA_GRACZY];
+	//inicjalizacja winsock
 	wersja = MAKEWORD(1, 1);
 	result = WSAStartup(wersja, &wsas);
-	int port;
-	printf("Podaj numer portu na ktorym chcesz stworzyc serwer: ");
-	scanf("%d", &port);
-	SOCKET s;
+	//pobranie nr portu
+	//printf("Podaj numer portu na ktorym chcesz stworzyc serwer: ");
+	//scanf("%d", &port);
+	//stworzenie gniazdka nasluchujacego
 	s = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in sa;
 	memset((void *)(&sa), 0, sizeof(sa));
 	sa.sin_family = AF_INET;
 
 	//int stary_port = 1337;
 	//int nowy_port = 19309;
 
-	sa.sin_port = htons(port);
+	//inicjalizacja i start serwera
+	sa.sin_port = htons(1337);//port);
 	sa.sin_addr.s_addr = htonl(INADDR_ANY);
-
 	result = bind(s, (struct sockaddr FAR*)&sa, sizeof(sa));
 	result = listen(s, 5);
 	printf("serwer uruchomiony. Oczekiwanie na graczy: \n");
-	SOCKET si[LICZBA_GRACZY];
 
-	struct sockaddr_in sc;
-	int lenc;
+	//przyjmowanie graczy
 	lenc = sizeof(sc);
 	si[0] = accept(s, (struct sockaddr FAR *) &sc, &lenc);
 	std::cout << "POLACZONO 1 gracza\n";
 	si[1] = accept(s, (struct sockaddr FAR *) &sc, &lenc);
 	std::cout << "POLACZONO 2 gracza\n";
-	//dddwdddddcllddccwddmddlllddddddddddddddddwddcccccdcdwdwddcdmdcdwwmdlllcmmmcdllddlddcdmdcddlddlddcmdmcddcdmdldcdddcddcmdddlcccccdddddddddddddmmmccmdmcccddmmmdcdddcdcddddd
-	//ddddddddddddddmmmmmmmmmmmddddddddddddddd1ddddddddd1dd2ddddddddd2dd3ddddd3dd33ddddddddddddddddddddddddddddmdmdmdmdmdmdddddddddddddddddddddddddddddddddddddddddmmmmmmmmmmmd
-	Gra gra1(2,si,"ddddddddddddddmmmmmmmmmmmddddddddddddddd1ddddddddd1dd2ddddddddd2dd3ddddd3dd33ddddddddddddddddddddddddddddmdmdmdmdmdmdddddddddddddddddddddddddddddddddddddddddmmmmmmmmmmmd");
-	std::cout << "Rozpoczecie rozgrywki\n";
-	closesocket(si[0]);
-	closesocket(si[1]);*/
 	
-	sqlite3 *db;
-	char *zErrMsg = 0;
-	int  rc;
-	char *sql;
-	/* Open database */
-	rc=sqlite3_open("test.db", &db);
+	// Open database 
+	rc = sqlite3_open("test.db", &db);
 	if (rc){
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 		exit(0);
@@ -82,19 +81,57 @@ int main()
 	else{
 		fprintf(stdout, "Opened database successfully\n");
 	}
-	//MAPY (ID, ZAWARTOSC)
-	/* Create SQL statement */
-	sql = "SELECT *	"  \
-		"	FROM MAPY";
-	/* Execute SQL statement */
+	// Create SQL statement 
+	sql = "SELECT ZAWARTOSC "  \
+		"	FROM MAPY " \
+		"	WHERE ID=1 ";
+	// Execute SQL statement 
 	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 	if (rc != SQLITE_OK){
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 	}
 	else{
-		fprintf(stdout, "Table created successfully\n");
+		fprintf(stdout, "Mapa pobrana\n");
 	}
 	sqlite3_close(db);
-	return 0;
+
+	//dddwdddddcllddccwddmddlllddddddddddddddddwddcccccdcdwdwddcdmdcdwwmdlllcmmmcdllddlddcdmdcddlddlddcmdmcddcdmdldcdddcddcmdddlcccccdddddddddddddmmmccmdmcccddmmmdcdddcdcddddd
+	//ddddddddddddddmmmmmmmmmmmddddddddddddddd1ddddddddd1dd2ddddddddd2dd3ddddd3dd33ddddddddddddddddddddddddddddmdmdmdmdmdmdddddddddddddddddddddddddddddddddddddddddmmmmmmmmmmmd
+	Gra gra1(2,si,mapa);
+	std::cout << "Rozpoczecie rozgrywki\n";
+	closesocket(si[0]);
+	closesocket(si[1]);
 }
+
+/*dodawanie elementow
+
+sqlite3 *db;
+char *zErrMsg = 0;
+int  rc;
+char *sql;
+// Open database 
+rc = sqlite3_open("test.db", &db);
+if (rc){
+	fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+	exit(0);
+}
+else{
+	fprintf(stdout, "Opened database successfully\n");
+}
+//MAPY (ID, ZAWARTOSC)
+// Create SQL statement 
+sql = "SELECT *	"  \
+"	FROM MAPY";
+// Execute SQL statement 
+rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+if (rc != SQLITE_OK){
+	fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	sqlite3_free(zErrMsg);
+}
+else{
+	fprintf(stdout, "Table created successfully\n");
+}
+sqlite3_close(db);
+return 0;
+*/
